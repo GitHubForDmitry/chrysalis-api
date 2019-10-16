5d7ac280df7ff4f47896fec030c47aee31354b7c
GitHubForDmitry <dmitrykoka@gmail.com>
2019-10-16 10:11:25 +0300

    add deutsch language

---
+++ packages/chrysalis-keymap/src/lib/languages/deutsch/deutsch.js
@@ -0,0 +1,264 @@
+/* chrysalis-keymap -- Chrysalis keymap library
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Is an Array of objects of values that have to be modified.
+ */
+
+const deutsch = [
+    {
+        code: 53,
+        labels: {
+            primary: "^"
+        }
+    },
+    {
+        code: 45,
+        labels: {
+            primary: "ß"
+        },
+        newGroupName: "Letters"
+    },
+    {
+        code: 46,
+        labels: {
+            primary: "´"
+        }
+    },
+    {
+        code: 28,
+        labels: {
+            primary: "Z"
+        }
+    },
+    {
+        code: 47,
+        labels: {
+            primary: "Ü"
+        },
+        newGroupName: "Letters"
+    },
+    {
+        code: 48,
+        labels: {
+            primary: "+"
+        }
+    },
+    {
+        code: 49,
+        labels: {
+            primary: "#"
+        }
+    },
+    {
+        code: 51,
+        labels: {
+            primary: "Ö"
+        },
+        newGroupName: "Letters"
+    },
+    {
+        code: 52,
+        labels: {
+            primary: "Ä"
+        },
+        newGroupName: "Letters"
+    },
+    {
+        code: 29,
+        labels: {
+            primary: "Y"
+        }
+    },
+    {
+        code: 56,
+        labels: {
+            primary: "-"
+        }
+    },
+    {
+        code: 1055,
+        labels: {
+            primary: "²"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1056,
+        labels: {
+            primary: "³"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1060,
+        labels: {
+            primary: "{"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1061,
+        labels: {
+            primary: "["
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1062,
+        labels: {
+            primary: "]"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1063,
+        labels: {
+            primary: "}"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1069,
+        labels: {
+            primary: "\\"
+        }
+    },
+    {
+        code: 1044,
+        labels: {
+            primary: "@"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1032,
+        labels: {
+            primary: "€"
+        },
+        newGroupName: "DigitTable"
+    },
+    {
+        code: 1072,
+        labels: {
+            primary: "~"
+        }
+    },
+    {
+        code: 1073,
+        labels: {
+            primary: "|"
+        }
+    },//button on iso keyboard
+    {
+        code: 1040,
+        labels: {
+            primary: "µ"
+        }
+    },
+    {
+        code: 2101,
+        labels: {
+            primary: "°"
+        }
+    },
+    {
+        code: 2079,
+        labels: {
+            primary: "\""
+        }
+    },
+    {
+        code: 2080,
+        labels: {
+            primary: "§"
+        }
+    },
+    {
+        code: 2083,
+        labels: {
+            primary: "&"
+        }
+    },
+    {
+        code: 2084,
+        labels: {
+            primary: "/"
+        }
+    },
+    {
+        code: 2085,
+        labels: {
+            primary: "("
+        }
+    },
+    {
+        code: 2086,
+        labels: {
+            primary: ")"
+        }
+    },
+    {
+        code: 2087,
+        labels: {
+            primary: "="
+        }
+    },
+    {
+        code: 2093,
+        labels: {
+            primary: "?"
+        }
+    },
+    {
+        code: 2094,
+        labels: {
+            primary: "`"
+        }
+    },
+    {
+        code: 2096,
+        labels: {
+            primary: "*"
+        }
+    },
+    {
+        code: 2097,
+        labels: {
+            primary: "'"
+        }
+    },
+    {
+        code: 2102,
+        labels: {
+            primary: ";"
+        }
+    },
+    {
+        code: 2103,
+        labels: {
+            primary: ":"
+        }
+    },
+    {
+        code: 2104,
+        labels: {
+            primary: "_"
+        }
+    },
+];
+
+export default deutsch;
