b21356b6bc8bbd26be2362cd8e4a34749af6f560
GitHubForDmitry <dmitrykoka@gmail.com>
2019-10-16 14:19:02 +0300

    add french language

---
+++ packages/chrysalis-keymap/src/lib/languages/french/french.js
@@ -0,0 +1,323 @@
+/* chrysalis-keymap -- Chrysalis keymap library
+ * Copyright (C) 2018  Keyboardio, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Is an Array of objects of values that have to be modified.
+ */
+
+const french = [
+  {
+    code: 47,
+    labels: {
+      primary: "²"
+    }
+  },
+  {
+    code: 30,
+    labels: {
+      primary: "&"
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 31,
+    labels: {
+      primary: "é"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 32,
+    labels: {
+      primary: "\""
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 33,
+    labels: {
+      primary: "'"
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 34,
+    labels: {
+      primary: "("
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 35,
+    labels: {
+      primary: "-"
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 36,
+    labels: {
+      primary: "è"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 37,
+    labels: {
+      primary: "_"
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 38,
+    labels: {
+      primary: "ç"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 39,
+    labels: {
+      primary: "à"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 45,
+    labels: {
+      primary: ")"
+    }
+  },
+  {
+    code: 20,
+    labels: {
+      primary: "A"
+    }
+  },
+  {
+    code: 26,
+    labels: {
+        primary: "Z"
+    }
+  },
+  {
+    code: 47,
+    labels: {
+      primary: "^"
+    }
+  },
+  {
+    code: 48,
+    labels: {
+      primary: "$"
+    }
+  },
+  {
+    code: 49,
+    labels: {
+      primary: "*"
+    }
+  },
+  {
+    code: 49,
+    labels: {
+      primary: "<"
+    }//button on iso keyboard
+  },
+  {
+    code: 4,
+    labels: {
+      primary: "Q"
+    }
+  },
+  {
+    code: 51,
+    labels: {
+      primary: "M"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 52,
+    labels: {
+      primary: "ù"
+    },
+    newGroupName: "Letters"
+  },
+  {
+    code: 29,
+    labels: {
+      primary: "W"
+    }
+  },
+  {
+    code: 29,
+    labels: {
+      primary: "W"
+    }
+  },
+  {
+    code: 16,
+    labels: {
+      primary: ","
+    },
+    newGroupName: "Punctuation"
+  },
+  {
+    code: 54,
+    labels: {
+      primary: ";"
+    }
+  },
+  {
+    code: 55,
+    labels: {
+      primary: ":"
+    }
+  },
+  {
+    code: 56,
+    labels: {
+      primary: "!"
+    }
+  },
+  {
+    code: 2078,
+    labels: {
+      primary: "1"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2079,
+    labels: {
+      primary: "2"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2080,
+    labels: {
+      primary: "3"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2081,
+    labels: {
+      primary: "4"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2082,
+    labels: {
+      primary: "5"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2083,
+    labels: {
+      primary: "6"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2084,
+    labels: {
+      primary: "7"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2085,
+    labels: {
+      primary: "8"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2086,
+    labels: {
+      primary: "9"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2087,
+    labels: {
+      primary: "0"
+    },
+    newGroupName: "Digits"
+  },
+  {
+    code: 2093,
+    labels: {
+      primary: "°"
+    }
+  },
+  {
+    code: 2095,
+    labels: {
+      primary: "¨"
+    }
+  },
+  {
+    code: 2096,
+    labels: {
+      primary: "£"
+    }
+  },
+  {
+    code: 2097,
+    labels: {
+      primary: "µ"
+    }
+  },
+  {
+    code: 2100,
+    labels: {
+      primary: "%"
+    }
+  },
+  {
+    code: 2064,
+    labels: {
+      primary: "S+,"
+    }
+  },
+  {
+    code: 2102,
+    labels: {
+      primary: "."
+    }
+  },
+  {
+    code: 2103,
+    labels: {
+      primary: "/"
+    }
+  },
+  {
+    code: 2104,
+    labels: {
+      primary: "§"
+    }
+  }
+];
+
+export default french;
